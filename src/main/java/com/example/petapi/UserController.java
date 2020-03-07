74961959a457656d5a2c22b68ba9651f8c2c451a
willerhe <willerhe@126.com>
2020-03-07 10:55:20 +0800

    添加登录注册功能

--- src/main/java/com/example/petapi/UserController.java
+++ src/main/java/com/example/petapi/UserController.java
@@ -5,6 +5,7 @@ import com.example.petapi.entity.UserDO;
 import com.example.petapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,15 @@ public class UserController {
     @GetMapping("/v1/hello")
     public JSONObject hello(UserDO userDO) {
         return Result.Success(userService.test(userDO));
+    }
+
+    @PostMapping("/v1/login")
+    public JSONObject login(UserDO userDO) {
+        return Result.Success(userService.login(userDO));
+    }
+
+    @PostMapping("/v1/register")
+    public JSONObject register(UserDO userDO) {
+        return Result.Success(userService.register(userDO));
     }
 }
--- src/main/java/com/example/petapi/service/UserService.java
+++ src/main/java/com/example/petapi/service/UserService.java
@@ -1,22 +1,47 @@
 package com.example.petapi.service;
 
+import com.alibaba.fastjson.JSONObject;
+import com.example.petapi.Result;
 import com.example.petapi.dao.UserDOMapper;
 import com.example.petapi.entity.UserDO;
+import com.example.petapi.entity.UserDOExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService {
     @Autowired
     UserDOMapper userDOMapper;
 
-   public UserDO test(UserDO userDO){
-       userDO.setId("123124");
-       userDO.setAccount("123123");
-       userDO.setIsDeleted(false);
-       userDOMapper.insert(userDO);
+    public UserDO test(UserDO userDO) {
+        userDO.setId("123124");
+        userDO.setAccount("123123");
+        userDO.setIsDeleted(false);
+        userDOMapper.insert(userDO);
+        return userDOMapper.selectByPrimaryKey("123124");
+    }
 
+    // 注册
+    public UserDO register(UserDO userDO) {
+        userDO.setId(UUID.randomUUID().toString());
+        userDOMapper.insert(userDO);
+        return userDOMapper.selectByPrimaryKey(userDO.getId());
+    }
 
-          return userDOMapper.selectByPrimaryKey("123124");
-   }
+    // 登录
+    public JSONObject login(UserDO userDO) {
+        UserDOExample e = new UserDOExample();
+        UserDOExample.Criteria c = e.createCriteria();
+        c.andPasswordEqualTo(userDO.getPassword());
+        c.andAccountEqualTo(userDO.getAccount());
+        List<UserDO> list = userDOMapper.selectByExample(e);
+        if (list.size() == 0) {
+            return Result.Failed("没有找到当前用户");
+        } else {
+            return Result.Success(list.get(0));
+        }
+    }
 }
